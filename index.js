2747eb3ceb9bd737f1841042e2e7c449ec5aa1d0
davidgzdeveloper <davidgzdeveloper@gmail.com>
2023-05-18 20:57:02 -0300

    added score and restart

--- index.js
+++ index.js
@@ -27,8 +27,9 @@ class Block {
     }
 }
 
-//all my blocks 
-const blocks = [
+let blocks = [];
+function addBlocks(){
+    blocks = [
     new Block(10, 270),
     new Block(120, 270),
     new Block(230, 270),
@@ -44,20 +45,16 @@ const blocks = [
     new Block(230, 210),
     new Block(340, 210),
     new Block(450, 210),
-]
+  ];
 
-//draw all my blocks
-function addBLocks(){
-    for(let i=0; i<blocks.length; i++){
-    const block = document.createElement('div')
-    block.classList.add('block')
-    block.style.left = blocks[i].bottomLeft[0] + 'px'
-    block.style.bottom = blocks[i].bottomLeft[1] + 'px' 
-    grid.appendChild(block)
+  blocks.forEach((block) =>{
+    const blockElement = document.createElement('div');
+    blockElement.classList.add('block');
+    blockElement.style.left = block.bottomLeft[0] + 'px';
+    blockElement.style.bottom = block.bottomLeft[1] + 'px';
+    grid.appendChild(blockElement);
+  })
     }
-}
-
-addBLocks()
 
 //add user
 const user = document.createElement('div')
@@ -92,6 +89,10 @@ function moveUser(e){
                 drawUser()
             }
             break;
+        case 'r':
+        case 'R':
+            restartGame();
+            break;
     }
 }
 
@@ -102,6 +103,8 @@ const ball = document.createElement('div')
 ball.classList.add('ball')
 drawBall()
 grid.appendChild(ball)
+
+drawBlocks();
 
 //move ball
 function moveBall(){
@@ -120,19 +123,24 @@ function checkForCollisions() {
     for(let i = 0; i < blocks.length; i++) {
         if
         (
-            (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0]) &&
-            ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1])
+            (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && 
+            ballCurrentPosition[0] < blocks[i].bottomRight[0]) &&
+            ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] &&
+             ballCurrentPosition[1] < blocks[i].topLeft[1])
         ) 
             {
-            const allBlocks = Array.from(document.querySelectorAll('.block'))
-            allBlocks[i].classList.remove('block')
-            blocks.splice(i,1) 
-            changeDirection()
+            const allBlocks = Array.from(document.querySelectorAll('.block'));
+            const collidedBlock = allBlocks[i];
+            collidedBlock.classList.remove('block')
+            collidedBlock.remove();
+            blocks.splice(i,1); 
+            changeDirection();
             score++
             scoreDisplay.innerHTML = score
+            
             //check for win 
             if(blocks.length == 0 ) {
-                 scoreDisplay.innerHTML = 'YOU WIN'
+                 window.alert('YOU WIN!!!')
                  clearInterval(timerId)
                  document.removeEventListener('keydown', moveUser)
             }
@@ -164,8 +172,12 @@ function checkForCollisions() {
     //check for game over
     if (ballCurrentPosition[1] <= 0) {
         clearInterval(timerId)
-        scoreDisplay.innerHTML = 'You lose'
-        document.removeEventListener('keydown', moveUser)
+        window.alert('YOU LOSE :(. PRESS R TO RESTART')
+        document.addEventListener('keydown', function (e) {
+            if (e.key === 'r' || e.key === 'R') {
+              restartGame();
+            }
+          });
     }
 }
 
@@ -187,3 +199,31 @@ function changeDirection() {
         return
     }
 } 
+
+function restartGame(){
+    currentPosition = userStart;
+    ballCurrentPosition = ballStart;
+    xDirection = -2;
+    yDirection = 2;
+    score = 0;
+    scoreDisplay.innerHTML = score;
+    
+    const allBlocks = document.querySelectorAll('.block');
+    allBlocks.forEach((block) => block.remove());
+
+    
+    addBlocks();
+    drawUser();
+    drawBall();
+
+  clearInterval(timerId);
+  timerId = setInterval(moveBall, 30);
+    
+
+}
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'r' || e.key === 'R') {
+      restartGame();
+    }
+  });
